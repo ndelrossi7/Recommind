bd59347d7f9c97cc372652ad4f665610218ca274
Natalie <natalie.delrossi@mail.mcgill.ca>
2020-05-04 19:50:57 -0400

    read data

---
+++ functions.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# dataframe of all vectorized practitioners
+vectorized = pd.read_csv('recdf.csv')
+
+# dataframe of cleaned practitioner info for frontend
+response = pd.read_csv('response_df.csv')
